ce2f0f4ef46375e5b7cb436e793204a48df8b45a
Saurabh Sandav <justmailsaurabh@gmail.com>
2018-08-21 20:35:17 +0530

    Extensions: Add asMutable()
    Signed-off-by: Saurabh Sandav <justmailsaurabh@gmail.com>

--- app/src/main/java/com/redridgeapps/remoteforqbittorrent/util/Extensions.kt
+++ app/src/main/java/com/redridgeapps/remoteforqbittorrent/util/Extensions.kt
@@ -1,6 +1,8 @@
 package com.redridgeapps.remoteforqbittorrent.util
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProviders
 import com.redridgeapps.remoteforqbittorrent.ui.base.BaseFragment
@@ -10,4 +12,8 @@ val BaseFragment.compatActivity
 
 fun <T : ViewModel> BaseFragment.getViewModel(viewModelClass: Class<T>): T {
     return ViewModelProviders.of(this, viewModelFactory).get(viewModelClass)
+}
+
+fun <T> LiveData<T>.asMutable(): MutableLiveData<T> {
+    return this as MutableLiveData<T>
 }
