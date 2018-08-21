4d96b64470fcc3627a2368956ac9351505be394d
Saurabh Sandav <justmailsaurabh@gmail.com>
2018-08-21 16:18:22 +0530

    PreferenceRepository: Add initialConfigFinished pref
    Signed-off-by: Saurabh Sandav <justmailsaurabh@gmail.com>

--- app/src/main/java/com/redridgeapps/remoteforqbittorrent/repo/PreferenceRepository.kt
+++ app/src/main/java/com/redridgeapps/remoteforqbittorrent/repo/PreferenceRepository.kt
@@ -2,6 +2,8 @@ package com.redridgeapps.remoteforqbittorrent.repo
 
 import android.content.SharedPreferences
 import android.content.res.Resources
+import com.redridgeapps.remoteforqbittorrent.R
+import com.redridgeapps.remoteforqbittorrent.util.delegate.BooleanPrefDelegate
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -9,4 +11,13 @@ import javax.inject.Singleton
 class PreferenceRepository @Inject constructor(
         res: Resources,
         prefs: SharedPreferences
-)
+) {
+
+    var initialConfigFinished by BooleanPrefDelegate(
+            prefs,
+            res.getString(R.string.key_pref_initial_config_finished),
+            DEFAULT_INITIAL_CONFIG_FINISHED
+    )
+}
+
+private const val DEFAULT_INITIAL_CONFIG_FINISHED = false
