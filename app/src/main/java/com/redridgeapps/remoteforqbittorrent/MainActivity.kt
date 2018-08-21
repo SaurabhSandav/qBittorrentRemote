298f9d5f551b56743c8ef236436f13e02346aa5b
Saurabh Sandav <justmailsaurabh@gmail.com>
2018-08-21 17:25:47 +0530

    MainActivity: Implement Navigation
    Signed-off-by: Saurabh Sandav <justmailsaurabh@gmail.com>

--- app/src/main/java/com/redridgeapps/remoteforqbittorrent/MainActivity.kt
+++ app/src/main/java/com/redridgeapps/remoteforqbittorrent/MainActivity.kt
@@ -5,9 +5,15 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.android.material.navigation.NavigationView
 import com.redridgeapps.remoteforqbittorrent.databinding.ActivityMainBinding
+import com.redridgeapps.remoteforqbittorrent.repo.PreferenceRepository
 import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
@@ -21,15 +27,21 @@ class MainActivity : AppCompatActivity(),
     @Inject
     lateinit var fragmentDispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
 
+    @Inject
+    lateinit var prefRepo: PreferenceRepository
+
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
+        navController = findNavController(R.id.nav_host_fragment)
 
         setSupportActionBar(binding.toolbar)
+        setupNavigation()
         setupDrawer()
     }
 
@@ -41,12 +53,31 @@ class MainActivity : AppCompatActivity(),
         } else super.onBackPressed()
     }
 
+    override fun onSupportNavigateUp() = binding.drawerLayout.navigateUp(navController)
+
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         binding.drawerLayout.closeDrawer(GravityCompat.START)
         return true
     }
 
+    private fun setupNavigation() {
+        try {
+            navController.graph
+        } catch (e: IllegalStateException) {
+            val graphResId = if (!prefRepo.initialConfigFinished) R.navigation.config_graph else R.navigation.nav_graph
+            navController.setGraph(graphResId)
+        }
+
+        navController.addOnNavigatedListener { _, destination ->
+            if (destination.id == R.id.torrentListFragment)
+                binding.drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            else
+                binding.drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+        }
+    }
+
     private fun setupDrawer() {
         binding.navView.setNavigationItemSelectedListener(this)
+        setupActionBarWithNavController(navController, binding.drawerLayout)
     }
 }
--- app/src/main/java/com/redridgeapps/remoteforqbittorrent/ui/config/ConfigFragment.kt
+++ app/src/main/java/com/redridgeapps/remoteforqbittorrent/ui/config/ConfigFragment.kt
@@ -1,5 +1,6 @@
 package com.redridgeapps.remoteforqbittorrent.ui.config
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,12 +9,18 @@ import androidx.databinding.DataBindingUtil
 import com.redridgeapps.remoteforqbittorrent.R
 import com.redridgeapps.remoteforqbittorrent.databinding.FragmentConfigBinding
 import com.redridgeapps.remoteforqbittorrent.ui.base.BaseFragment
+import com.redridgeapps.remoteforqbittorrent.util.compatActivity
 import com.redridgeapps.remoteforqbittorrent.util.getViewModel
 
 class ConfigFragment : BaseFragment() {
 
     private lateinit var binding: FragmentConfigBinding
     private lateinit var viewModel: ConfigViewModel
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        compatActivity.supportActionBar?.setDisplayHomeAsUpEnabled(false)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
