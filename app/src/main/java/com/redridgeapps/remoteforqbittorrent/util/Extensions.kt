534eca692e02132d7a0d2ba1d14fc8198e171168
Saurabh Sandav <justmailsaurabh@gmail.com>
2018-08-21 15:07:04 +0530

    Extensions: Add BaseFragment.getViewModel()
    Signed-off-by: Saurabh Sandav <justmailsaurabh@gmail.com>

--- app/src/main/java/com/redridgeapps/remoteforqbittorrent/util/Extensions.kt
+++ app/src/main/java/com/redridgeapps/remoteforqbittorrent/util/Extensions.kt
@@ -1,1 +1,9 @@
 package com.redridgeapps.remoteforqbittorrent.util
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
+import com.redridgeapps.remoteforqbittorrent.ui.base.BaseFragment
+
+fun <T : ViewModel> BaseFragment.getViewModel(viewModelClass: Class<T>): T {
+    return ViewModelProviders.of(this, viewModelFactory).get(viewModelClass)
+}
