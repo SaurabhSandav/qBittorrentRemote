b44605eba5c6d769b0c5ab5054635a9bce148bc9
Saurabh Sandav <justmailsaurabh@gmail.com>
2018-08-21 14:33:56 +0530

    MainActivity: Implement layout with drawer
    Signed-off-by: Saurabh Sandav <justmailsaurabh@gmail.com>

--- app/src/main/java/com/redridgeapps/remoteforqbittorrent/MainActivity.kt
+++ app/src/main/java/com/redridgeapps/remoteforqbittorrent/MainActivity.kt
@@ -1,24 +1,52 @@
 package com.redridgeapps.remoteforqbittorrent
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
+import com.redridgeapps.remoteforqbittorrent.databinding.ActivityMainBinding
 import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
+class MainActivity : AppCompatActivity(),
+        HasSupportFragmentInjector,
+        NavigationView.OnNavigationItemSelectedListener {
 
     @Inject
     lateinit var fragmentDispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
+
+        setSupportActionBar(binding.toolbar)
+        setupDrawer()
     }
 
     override fun supportFragmentInjector(): AndroidInjector<Fragment> = fragmentDispatchingAndroidInjector
+
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else super.onBackPressed()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun setupDrawer() {
+        binding.navView.setNavigationItemSelectedListener(this)
+    }
 }
