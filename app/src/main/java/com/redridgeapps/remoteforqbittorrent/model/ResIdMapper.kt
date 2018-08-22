76a85219d468a6d29ecf6d94c4ab1bbcde89ede6
Saurabh Sandav <justmailsaurabh@gmail.com>
2018-08-22 13:04:51 +0530

    ResIdMapper: Add TorrentState.toResId()
    Signed-off-by: Saurabh Sandav <justmailsaurabh@gmail.com>

--- app/src/main/java/com/redridgeapps/remoteforqbittorrent/model/ResIdMapper.kt
+++ app/src/main/java/com/redridgeapps/remoteforqbittorrent/model/ResIdMapper.kt
@@ -1,3 +1,21 @@
 package com.redridgeapps.remoteforqbittorrent.model
 
-object ResIdMapper
+import com.redridgeapps.remoteforqbittorrent.R
+
+object ResIdMapper {
+
+    fun TorrentState.toResId() = when (this) {
+        TorrentState.ERROR -> R.string.state_error
+        TorrentState.PAUSED_UP -> R.string.state_paused_up
+        TorrentState.PAUSED_DL -> R.string.state_paused
+        TorrentState.QUEUED_UP -> R.string.state_queued_up
+        TorrentState.QUEUED_DL -> R.string.state_queued_dl
+        TorrentState.UPLOADING -> R.string.state_seeding
+        TorrentState.STALLED_UP -> R.string.state_stalled_up
+        TorrentState.CHECKING_UP -> R.string.state_checking
+        TorrentState.CHECKING_DL -> R.string.state_checking
+        TorrentState.DOWNLOADING -> R.string.state_downloading
+        TorrentState.STALLED_DL -> R.string.state_stalled_dl
+        TorrentState.META_DL -> R.string.state_fetching_metadata
+    }
+}
