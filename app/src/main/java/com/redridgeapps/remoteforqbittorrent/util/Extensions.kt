5968d212a6b18ff9193bbb47ecba85b1d598a103
Saurabh Sandav <justmailsaurabh@gmail.com>
2018-08-22 12:42:36 +0530

    Extensions: Add Long.humanReadableByteCount()
    Signed-off-by: Saurabh Sandav <justmailsaurabh@gmail.com>

--- app/src/main/java/com/redridgeapps/remoteforqbittorrent/util/Extensions.kt
+++ app/src/main/java/com/redridgeapps/remoteforqbittorrent/util/Extensions.kt
@@ -17,3 +17,21 @@ fun <T : ViewModel> BaseFragment.getViewModel(viewModelClass: Class<T>): T {
 fun <T> LiveData<T>.asMutable(): MutableLiveData<T> {
     return this as MutableLiveData<T>
 }
+
+fun Long.humanReadableByteCount(isSpeed: Boolean = false, si: Boolean = false): String {
+    val bytes = this
+    val unit = if (si) 1000 else 1024
+    val speed = if (isSpeed) "/s" else ""
+
+    if (bytes < unit) return bytes.toString() + " B" + speed
+
+    val exp = (Math.log(bytes.toDouble()) / Math.log(unit.toDouble())).toInt()
+    val pre = (if (si) "kMGTPE" else "KMGTPE")[exp - 1] + if (si) "" else "i"
+
+    return String.format(
+            "%.1f %sB%s",
+            bytes / Math.pow(unit.toDouble(), exp.toDouble()),
+            pre,
+            speed
+    )
+}
