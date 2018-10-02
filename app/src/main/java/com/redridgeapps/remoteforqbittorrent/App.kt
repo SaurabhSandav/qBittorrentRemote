5c9bf87ada366d434ccb78ef76fabbf1669fb4b4
Saurabh Sandav <justmailsaurabh@gmail.com>
2018-10-02 16:20:59 +0530

    Add/Implement LeakCanary
    Signed-off-by: Saurabh Sandav <justmailsaurabh@gmail.com>

--- app/src/main/java/com/redridgeapps/remoteforqbittorrent/App.kt
+++ app/src/main/java/com/redridgeapps/remoteforqbittorrent/App.kt
@@ -1,5 +1,14 @@
 package com.redridgeapps.remoteforqbittorrent
 
 import android.app.Application
+import com.squareup.leakcanary.LeakCanary
 
-class App : Application()
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (LeakCanary.isInAnalyzerProcess(this@App)) return
+        LeakCanary.install(this)
+    }
+}
