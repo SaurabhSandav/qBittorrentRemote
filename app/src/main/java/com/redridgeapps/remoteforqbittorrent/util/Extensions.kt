8d1f127af0514e94f37009464df64d4932982b1b
Saurabh Sandav <justmailsaurabh@gmail.com>
2018-10-02 19:06:36 +0530

    Extensions: Add Fragment.compatActivity
    Signed-off-by: Saurabh Sandav <justmailsaurabh@gmail.com>

--- app/src/main/java/com/redridgeapps/remoteforqbittorrent/util/Extensions.kt
+++ app/src/main/java/com/redridgeapps/remoteforqbittorrent/util/Extensions.kt
@@ -1,8 +1,12 @@
 package com.redridgeapps.remoteforqbittorrent.util
 
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProviders
 import com.redridgeapps.remoteforqbittorrent.ui.base.BaseFragment
+
+val BaseFragment.compatActivity
+    get() = requireActivity() as AppCompatActivity
 
 fun <T : ViewModel> BaseFragment.getViewModel(viewModelClass: Class<T>): T {
     return ViewModelProviders.of(this, viewModelFactory).get(viewModelClass)
